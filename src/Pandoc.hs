f2b1f6d67faab303ba8bf9762288bcea2e8aa634
d12frosted <d12frosted@icloud.com>
2015-08-09 16:08:44 +0300

    convert org date to hakyll date

--- src/Pandoc.hs
+++ src/Pandoc.hs
@@ -24,7 +24,18 @@ transformPandoc p = meta <> p
         meta = transformMeta . getMetaMap $ p
 
 transformMeta :: Map String MetaValue -> Pandoc
-transformMeta = doc . wrapInBlock . foldMapWithKey (\k v -> toMeta k $ stringify v)
+transformMeta = doc . wrapInBlock . foldMapWithKey (\k v -> toMeta k . maybeConvert k . stringify $ v)
   where blockSep = str "---\n"
         toMeta k v = str k <> str ": " <> str v <> str "\n"
         wrapInBlock inl = plain $ blockSep <> inl <> blockSep
+        maybeConvert k v =
+          if k == "date"
+             then convert v
+             else v
+        convert = stripLeft "<" . stripRight ">"
+
+stripLeft :: String -> String -> String
+stripLeft a b = if a `isPrefixOf` b then drop (length a) b else b
+
+stripRight :: String -> String -> String
+stripRight a b = if a `isSuffixOf` b then take (length b - length a) b else b
