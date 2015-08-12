b904da016f597ceab724d1cc733d5579b49619a0
d12frosted <d12frosted@icloud.com>
2015-08-12 10:24:37 +0300

    push site to master

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ cd _site
 git init
 git remote add origin git@github.com:d12frosted/d12frosted.github.io.git
 git fetch origin
-git checkout site
+git checkout master
 git add .
 git commit -m "snapshot $(date '+%m/%d/%y %H:%M')"
-git push origin site
+git push origin master
